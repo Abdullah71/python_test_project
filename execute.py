8a38b93586f76a15e86639fbf739fb78790c6d90
Abdullah <abdullah.alhossain@gmail.com>
2025-06-08 22:16:58 +0600

    Update execute.py to dynamically run all Python scripts in math folder

--- execute.py
+++ execute.py
@@ -1,13 +1,20 @@
 import subprocess
 import sys
+import os
 
 # List of math operation scripts to run in order
-scripts = ["add.py", "substract.py", "multiply.py", "divide.py"]
+# scripts = ["add.py", "substract.py", "multiply.py", "divide.py"]
+math_folder = "math"
+scripts = sorted(
+    f for f in os.listdir(math_folder)
+    if f.endswith('.py') and os.path.isfile(os.path.join(math_folder, f))
+)
 
 for script in scripts:
+    script_path = os.path.join(math_folder, script)
     try:
         result = subprocess.run(
-            [sys.executable, script],
+            [sys.executable, script_path],
             capture_output=True,
             text=True,
             check=False
